2834d8b32ba6d7aa6f1a5a54019d343d42ba5950
seohsj <hyunju6889@naver.com>
2020-05-25 22:17:45 +0900

    '

--- ch7_graph/16929/16929.cpp
+++ ch7_graph/16929/16929.cpp
@@ -1,42 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<string>
-#include<queue>
-#include<cstdio>
+#include<cstring>
 #include<iostream>
 using namespace std;
 int n, m;
 string board[55];
-int group[55][55];  //0방문 안함 , 몇 번째로 방문한 노드인지
 int dx[] = { 1,0,-1,0 };
 int dy[] = { 0,1,0,-1 };
-pair<int, int>before[55][55]; //바로 이전에 방문한 노드
-bool bfs(int firstx, int firsty,char ch) {  //ch 방문 할 문자 , x y 시작 좌표
-	queue<pair<int,int>> que;
-	que.push({ firstx,firsty });
-	group[firstx][firsty] = 1;
-	while (!que.empty()) {
-		int x = que.front().first;
-		int y = que.front().second;
-		que.pop();
-		bool connect = false;
+int dist[55][55];       //시작점에서 x,y까지 거리(최단 거리가 아님)
+bool check[55][55];     //x,y를 방문했는지 여부
+bool dfs(int x, int y, int cnt, char color) {  //cnt 방문한 점의 개수
+	if (check[x][y]) {
+		if (cnt - dist[x][y] >= 4) return true;
+		else return false;
+	}
 
-		for (int i = 0; i < 4; i++) {
-			int nextX = x + dx[i];
-			int nextY = y + dy[i];
-			if (nextX >= 0 || nextY >= 0 || nextX <= m || nextY <= n) {
-				if (group[nextX][nextY] >= 4 && board[nextX][nextY] == ch && group[nextX][nextY] !=0) return true;
-
-				if (board[nextX][nextY] == ch && group[nextX][nextY] ==0) {
-					que.push({ nextX,nextY });
-					group[nextX][nextY] = group[x][y]+1;
-
-				}
-
-
+	check[x][y] = true;
+	dist[x][y] = cnt;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (nx >= 0 && ny >= 0 && nx < n && ny < m && color==board[nx][ny]) {
+			if (dfs(nx, ny, cnt + 1, color)) {
+				return true;
 			}
-
 		}
-
 	}
 	return false;
 }
@@ -48,16 +35,16 @@ int main() {
 	}
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (group[i][j] == 0) { 
-				bool ans=bfs(i, j, board[i][j]); 
-				if (ans == true) {
-					cout << "YES" << "\n";
-					return 0;
-				}
+			if (check[i][j]) continue;
+			memset(dist, 0, sizeof(dist));
+			bool ok = dfs(i, j, 1, board[i][j]);
+			if (ok) {
+				cout << "Yes" << "\n";
+				return 0;
 			}
-
 		}
 	}
-	cout << "NO" << "\n";
+	cout << "No" << "\n";
 	return 0;
+
 }
